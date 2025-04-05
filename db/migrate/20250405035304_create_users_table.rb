e0398e2b9673ceddaa0f396708a8e0d1730aec8c
Gagansagar108 <79242532+Gagansagar108@users.noreply.github.com>
2025-04-05 11:04:43 +0530

    commit

--- db/migrate/20250405035304_create_users_table.rb
+++ db/migrate/20250405035304_create_users_table.rb
@@ -2,12 +2,13 @@ class CreateUsersTable < ActiveRecord::Migration[8.0]
   def change
     create_table :users_tables do |t|
       t.string :name
-      t.string :email, index: true, null: false, unique: true
+      t.string :email, null: false
       t.integer :age 
       t.string :mobile
       t.string :mobile_country_code
       t.string :status, default: true
       t.timestamps
     end
+    add_index :users, :email, unique: true
   end
 end
