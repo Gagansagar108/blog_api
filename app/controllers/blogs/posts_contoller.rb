a12014a32dc2ca2a63e3b76355560314c7a4888f
Gagansagar108 <79242532+Gagansagar108@users.noreply.github.com>
2025-04-05 14:04:59 +0530

    c

--- app/controllers/blogs/posts_contoller.rb
+++
@@ -1,9 +0,0 @@
-module Blogs 
-class PostsController < ApplicationController
-    def health_check
-        return render json: { health: "good"}, status: 200
-    end 
-end 
-end 
-
-
---
+++ app/controllers/blogs/posts_controller.rb
@@ -0,0 +1,9 @@
+module Blogs 
+    class PostsController < ApplicationController
+        def health_check
+            return render json: { health: "good"}, status: 200
+        end 
+    end 
+end 
+
+
