224af1ab8589fd6616acba24ff3cc59fcddda032
Gagansagar108 <79242532+Gagansagar108@users.noreply.github.com>
2025-04-05 17:09:02 +0530

    r

--- config/application.rb
+++ config/application.rb
@@ -16,8 +16,11 @@ module BlogApi
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
 
+    config.eager_load_paths << Rails.root.join('app/middleware')
+    
     config.middleware.use RequestLogger
 
+    
     # Configuratrrion for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
