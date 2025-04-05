3e19771d1564438623394d84200f6e7db416c995
Gagansagar108 <79242532+Gagansagar108@users.noreply.github.com>
2025-04-05 14:03:34 +0530

    f

--- app/controllers/blogs/posts_contoller.rb
+++ app/controllers/blogs/posts_contoller.rb
@@ -1,7 +1,9 @@
-class Blogs::PostsController < ApplicationController
+module Blogs 
+class PostsController < ApplicationController
     def health_check
         return render json: { health: "good"}, status: 200
     end 
+end 
 end 
 
 
