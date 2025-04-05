6a5e9bb2caf2bda7e7ea255708ef0781f4feb987
Gagansagar108 <79242532+Gagansagar108@users.noreply.github.com>
2025-04-05 14:50:45 +0530

    minor

--- app/controllers/blogs/posts_controller.rb
+++ app/controllers/blogs/posts_controller.rb
@@ -1,23 +1,24 @@
-module Blogs 
-    class PostsController < ApplicationController
-        def health_check
-            return render json: { health: "good"}, status: 200
+
+class Blogs::PostsController < ApplicationController
+    def health_check
+        return render json: { health: "good"}, status: 200
+    end 
+
+    def create
+        post = Post.new(permit_params)
+        binding.pry
+        unless post.save 
+            return render json: { message: 'something went wrong'}, status: 422
         end 
 
-        def create
-            post = Post.new(permit_params)
+        render json: { message: "good"}, status: 200
+    end 
 
-            unless post.save 
-                return render json: { message: 'something went wrong'}, status: 422
-            end 
 
-            render json: { message: "good"}, status: 200
-        end 
-
-        def permit_params 
-            params.permit(:title, :body, :user_id)
-        end 
+    def permit_params 
+        params.permit(:title, :body, :user_id)
     end 
 end 
+
 
 
