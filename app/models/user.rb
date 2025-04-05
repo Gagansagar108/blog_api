8ff0b9d0c3d9912673645d5375bbf01fb1a41790
Gagansagar108 <79242532+Gagansagar108@users.noreply.github.com>
2025-04-05 15:05:52 +0530

    nice

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,27 @@
 class User < ApplicationRecord
      has_many :posts 
      has_many :comments
+
+     after_save :after_save_handling
+     before_save :before_save_handling
+
+     after_commit :after_commit_handling
+
+     after_create :after_create_handling
+
+     def after_save_handling
+          raise ActiveRecord::RecordInvalid, "after_save"
+     end 
+
+     def before_save_handling
+          raise ActiveRecord::RecordInvalid, "before_save_handling"
+     end 
+
+     def after_commit_handling
+          raise ActiveRecord::RecordInvalid, "after_commit_handling"
+     end 
+
+     def after_create_handling 
+          raise ActiveRecord::RecordInvalid, "after_create_handling"
+     end 
 end 
