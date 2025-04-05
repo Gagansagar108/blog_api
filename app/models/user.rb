ff375fa190b4773f2f3ee290282cc99bef30b2d4
Gagansagar108 <79242532+Gagansagar108@users.noreply.github.com>
2025-04-05 15:15:26 +0530

    minor

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,18 +11,26 @@ class User < ApplicationRecord
      after_create :after_create_handling
 
      def after_save_handling
-          raise ActiveRecord::RecordInvalid.new("after_save")
+          binding.pry
+          errors.add(:base, "after_save_handling")
+          raise ActiveRecord::RecordInvalid.new(self)
      end 
 
      def before_save_handling
-          raise ActiveRecord::RecordInvalid.new("before_save_handling")
+          binding.pry
+          errors.add(:base, "before_save_handling")
+          raise ActiveRecord::RecordInvalid.new(self)
      end 
 
      def after_commit_handling
-          raise ActiveRecord::RecordInvalid.new("after_commit_handling")
+          binding.pry
+          errors.add(:base, "after_commit_handling")
+          raise ActiveRecord::RecordInvalid.new(self)
      end 
 
      def after_create_handling 
-          raise ActiveRecord::RecordInvalid.new("after_create_handling")
+          binding.pry
+          errors.add(:base, "after_create_handling")
+          raise ActiveRecord::RecordInvalid.new(self)
      end 
 end 
