01346473060a674f63fb9ff06b77e47bef3ee629
Gagansagar108 <79242532+Gagansagar108@users.noreply.github.com>
2025-04-05 17:11:48 +0530

    t

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ module BlogApi
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
-t
+
     config.middleware.use RequestLogger
 
     
